361e38aaca78d35383e9e15ab7f997a78d65bbc6
Koshi-v-dushe <74434256+Koshi-v-dushe@users.noreply.github.com>
2020-12-02 18:45:28 +0300

    Update dev.sh

--- dev.sh
+++ dev.sh
@@ -3,5 +3,7 @@
 # This script exists, because https://github.com/GoogleContainerTools/skaffold/issues/4641
 
 kubectl --context minikube create -f common
-skaffold build --tag=latest --profile=minikube-profile
-skaffold dev --tag="latest" --profile=minikube-profile
+skaffold build --tag=latest --kube-context minikube
+skaffold run --tag="latest" --kube-context minikube
+
+
