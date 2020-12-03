7c2899092a0b63f890c62278eae25d0355f29a84
Koshi-v-dushe <74434256+Koshi-v-dushe@users.noreply.github.com>
2020-12-04 00:34:32 +0300

    fix clickhouse build problem
    this change fixes clickhous cache "UNAUTHORIZED"

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 # This script exists, because https://github.com/GoogleContainerTools/skaffold/issues/4641
 
 kubectl --context minikube create -f common
-skaffold build --tag=latest --kube-context minikube
-skaffold run --tag="latest" --kube-context minikube
+sudo skaffold build --tag=latest --kube-context minikube
+sudo skaffold run --tag="latest" --kube-context minikube
