ac593f21e1e82c0278744d2c7dc4e84bf7c3e127
Koshi-v-dushe <74434256+Koshi-v-dushe@users.noreply.github.com>
2020-12-03 21:37:46 +0300

    Revert "modify skaffold run yaml/script"
    This reverts commit 0e5cbeaeed9ab4b94f2b54a2327969803c520fc2.

--- dev.sh
+++ dev.sh
@@ -3,6 +3,5 @@
 # This script exists, because https://github.com/GoogleContainerTools/skaffold/issues/4641
 
 kubectl --context minikube create -f common
-skaffold build --tag=latest --kube-context minikube
-skaffold run --tag="latest" --kube-context minikube
-$SHELL
+skaffold build --tag=latest --profile=minikube-profile
+skaffold dev --tag="latest" --profile=minikube-profile
